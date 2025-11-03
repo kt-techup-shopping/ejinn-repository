84f87879fc9f8df03fe3cda1493b795a73b94470
ejinn1 <ejinn1@naver.com>
2025-11-03 09:22:36 +0900

    feat: 아이디 중복 체크

--- src/main/java/com/kt/repository/UserRepository.java
+++ src/main/java/com/kt/repository/UserRepository.java
@@ -54,4 +54,15 @@ public class UserRepository {
 
 		return maxId == null ? 0L : maxId;
 	}
+
+	// 아이디 중복 체크
+	// 1. count 해서 0보다 큰지 체크, select 해서 결과가 있는데 체크 -> 별로임
+	// 2. unique 제약조건 걸어서 예외 처리 -> (DateViolatuion Exception) 별로인듯
+	// 3. exist로 존재 여부 체크 -> boolean으로 바로 알 수 있음
+
+	 public boolean existByLoginId(String loginId) {
+		 var sql = "SELECT EXIST (SELECT id FROM MEMBER WHERE loginId = ?)";
+
+		 return Boolean.TRUE.equals(jdbcTemplate.queryForObject(sql, Boolean.class));
+	 }
 }
